6d1194ff0d597198241836550e9fa4cf6e678ca1
lezhumain <julien.merion@epsi.fr>
2016-02-05 20:14:39 +0100

    TcpServer working with 1 client

--- project/OGlobal.hpp
+++ project/OGlobal.hpp
@@ -1,78 +1,12 @@
 #pragma once
-
-#include <deque>
-#include "pthread.h"
-#include "TcpServer.hpp"
-#include "PasswordChunk.h"
-
-#define THREAD_COUNT 5
+#include <queue>
 
 
-
-class OGlobal
+template <class T>
+class Fifo<T> : public std::queue<T>
 {
-	struct givenChunk_t {
-		std::string startPass;
-		std::string agentIp;
-		int			port;
-	};
-
-	struct keyboardThreadArgs_t {
-		bool *appRunning;
-	};
-
-	public:
-		static OGlobal* GetInstance(const int nbThread = -1, const int chunkSize = -1, const std::string algo = "", const std::string hash = "", const std::string alphabet = "");
-		static void Kill();
-
-		const std::string GetAlphabet() const;
-		const std::string GetAlgo() const;
-		const std::string GetHash() const;
-		const std::string GetNextChunkBegin() const;
-		const std::string CraftResponse(const std::string request);
-		void StartServer(int port);
-		void StartKeyboardThread();
-
-
-		// TODO: make private
-		std::string generateChunk(const  std::string begin);
-		std::string getBeginFromEnd(const  std::string end);
-		int IndexOf(char letter);
-
-		//void replyChunk();
-
-
-
-	private:
-		static OGlobal *_instance;
-
-		const std::string _hash;
-		const std::string _alphabet;
-		const int _chunkSize;
-		const std::string _algo;
-
-		bool _appRunning;
-		TcpServer _server;
-		CPasswordChunk _nextChunk;
-		std::deque<givenChunk_t> _givenChunks;
-		pthread_t _keyboardThread;
-
-
-		OGlobal(const int nbThread, const int chunkSize, const std::string algo, const std::string hash, const std::string alphabet);
-		OGlobal& operator= (const OGlobal&) {} // TODO
-		~OGlobal();
-		static void *ThreadKeyboardFunc(void *p_arg);
-
-		
-		
-		//OGlobal(const OGlobal&) {} //TODO
-
-
-
-		//int _threadIds[THREAD_COUNT]; // TODO: change max
-		//pthread_t _threads[THREAD_COUNT];
-
-		//void createThreads();
-		//void *threadFunction()
+public:
+	Fifo<T>();
+	~Fifo<T>();
 };
 
