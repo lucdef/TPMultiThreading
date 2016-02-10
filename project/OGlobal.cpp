d92cb2281f8d7caf2d108499d17d409bf3a242df
lezhumain <julien.merion@epsi.fr>
2016-02-10 21:07:40 +0100

    generate chunk started

--- project/OGlobal.cpp
+++ project/OGlobal.cpp
@@ -2,11 +2,81 @@
 
 
 
-OGlobal::OGlobal()
+
+
+OGlobal::OGlobal(const int nbThread, const int chunkSize, std::string algo, std::string alphabet)
+	: _server(),
+	_alphabet(alphabet),
+	_chunkSize(chunkSize)
 {
+	// init threadId and thread array
+	//for (int i = 0; i < 5; ++i)
+	//{
+	//	_threadIds[i] = 0;
+	//	//_threads[i] = ;
+	//}
+
+	//createThreads();
 }
 
 
 OGlobal::~OGlobal()
 {
 }
+
+std::string OGlobal::generateChunk(std::string begin)
+{
+	static char lastInAlphabet = _alphabet[_alphabet.length() -1];
+
+	std::string end = begin;
+	int lastIndex = begin.length() - 1;
+
+	for (int i = lastIndex; i > lastIndex - _chunkSize; --i)
+	{
+		end[i] = lastInAlphabet;
+	}
+
+	std::cout << "generated : " << begin << " --> " << end << std::endl ;
+	//_nextChunk = 
+
+	return end;
+}
+
+std::string OGlobal::getBeginFromEnd(const std::string end)
+{
+	static char lastInAlphabet = _alphabet[_alphabet.length() - 1];
+	int lastIndex = end.length() - 1;
+	std::string begin = end;
+
+	int i = lastIndex;
+
+	while (begin[i] == lastInAlphabet)
+	{
+		begin.at(i) = _alphabet[0];
+		--i;
+	}
+	begin.at(i) = _alphabet[Index(begin[i]) + 1];
+
+	//std::cout << "Begin after " << end << " is " << begin << std::endl;
+
+
+	return begin;
+}
+
+int OGlobal::Index(char letter)
+{
+	int length = _alphabet.length();
+	
+	for (int i = 0; i < length; ++i)
+	{
+		if (_alphabet[i] == letter)
+			return i;
+	}
+
+	return -1;
+}
+
+//void OGlobal::createThreads()
+//{
+//
+//}
--- project/start.cpp
+++ project/start.cpp
@@ -9,6 +9,7 @@
 #include <Windows.h>
 
 #include "TcpTest.hpp"
+#include "OGlobalTest.hpp"
 
 void ExtractCommandLine( int argc, const char *argv[] )	{
 	// Command line settings
@@ -96,5 +97,9 @@ int main(int argc, const char *argv[]) {
 	//return EXIT_SUCCESS;
 
 	
-	return 	TcpTest::TestMain();
+	//return 	TcpTest::TestMain();
+	int res = OGlobalTest::TestMain();
+
+	std::cin.get();
+	return EXIT_SUCCESS;
 }
