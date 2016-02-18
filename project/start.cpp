ea2fcb3c04142e17b114bc563ed999f796148b99
lezhumain <julien.merion@epsi.fr>
2016-02-18 20:16:59 +0100

    thread keyboard ok

--- project/start.cpp
+++ project/start.cpp
@@ -97,8 +97,8 @@ int main(int argc, const char *argv[]) {
 	//return EXIT_SUCCESS;
 
 	
-	int res = TcpTest::TestMain();
-	//int res = OGlobalTest::TestMain();
+	//int res = TcpTest::TestMain();
+	int res = OGlobalTest::TestKeyboardThread();
 
 
 	std::cout << "\nAppuyer sur <Enter> pour continuer";
